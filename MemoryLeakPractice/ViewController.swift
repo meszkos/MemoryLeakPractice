61f248e2604c8b9b289d6ec3936fed9a54dda1d9
meszkos <michalik515@gmail.com>
2022-06-03 13:09:56 +0200

    Initial Commit

---
+++ MemoryLeakPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeakPractice
+//
+//  Created by MacOS on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
