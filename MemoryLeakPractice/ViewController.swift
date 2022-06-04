0c21573e916c5908c67393d8557d128c812278cc
meszkos <michalik515@gmail.com>
2022-06-04 12:21:57 +0200

    Completed tutorial

--- MemoryLeakPractice/ViewController.swift
+++ MemoryLeakPractice/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       let button = UIButton()
+        
+        button.setTitle("Tap Me", for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    
+    @objc private func didTapButton(){
+        let vc = SecondVC()
+        present(vc, animated: true)
+    }
 }
 
+
+
+class MyView: UIView{
+    weak var vc: UIViewController?
+    init(vc: UIViewController){
+        self.vc = vc
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+class SecondVC: UIViewController{
+    var myView: MyView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        myView = MyView(vc: self)
+    }
+}
